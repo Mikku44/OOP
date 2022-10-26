365ce536e001651a36b41be560a0c680ffe2971f
Mikku44 <104062911+Mikku44@users.noreply.github.com>
2022-10-26 19:42:49 +0700

    master

--- mini project/Gui.java
+++ mini project/Gui.java
@@ -6,14 +6,13 @@ public class Gui {
     public static void main(String[] args){
         JFrame frame  = new JFrame();
         JPanel panel = new JPanel();
-        JButton btn  = new JButton("Noodle");
+        JLabel title  = new JLabel("NOODLE NOOB");
+        panel.setBackground(Color.white);
+        panel.add(title);
+        title.setIcon(new ImageIcon("icon.png"));
         frame.setSize(800,600);
-        frame.setLayout(new GridLayout(0,1));
+        frame.add(panel,BorderLayout.NORTH);
 
-        panel.add(btn);
-
-
-        frame.add(panel);
         frame.setVisible(true);
 
     }
