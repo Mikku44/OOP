d1af1197d08ba49d8ad27c4188888e971321f60c
naruto <seeleendoli@gmail.com>
2022-10-27 11:20:30 +0700

    basileen
    1

--- mini project/Payment.java
+++ mini project/Payment.java
@@ -18,3 +18,20 @@ public class Payment {
     public void setchange(int change) {
         this.change = change;
     }
+    ////////////////////////////////////////////////////////////////////////
+
+    // Method สำหรับไว้ให้เรียกดู
+    public String getPayment() {
+        return this.typePayment;
+    }
+
+    // Method สำหรับไว้ให้เรียกดู
+    public String getPay() {
+        return this.pay;
+    }
+
+    // Method สำหรับไว้ให้เรียกดู
+    public String getchange() {
+        return this.change;
+    }
+}
