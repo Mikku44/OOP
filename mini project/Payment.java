51c42337110c6fd63bcd83afc6e7217e396860aa
naruto <seeleendoli@gmail.com>
2022-10-27 11:09:49 +0700

    Update Payment.java

---
+++ mini project/Payment.java
@@ -0,0 +1,20 @@
+public class Payment {
+    // กำหนดคุณสมบัติ
+    int typePayment;
+    int pay;
+    String change;
+
+    // Method สำหรับ
+    public void setPayment(int typePayment) {
+        this.typePayment = typePayment;
+    }
+
+    // Method สำหรับ
+    public void setPay(int pay) {
+        this.pay = pay;
+    }
+
+    // Method สำหรับ
+    public void setchange(int change) {
+        this.change = change;
+    }
